349ec16f8136dd4813ed99f49b50befd958f46b3
Juan Natib <juan.natib@correounivalle.edu.co>
2024-11-17 18:49:53 -0500

    making functional skeleton of the proyect

---
+++ src/main/java/com/example/miniproyect3fpoe/controller/WelcomeController.java
@@ -0,0 +1,31 @@
+package com.example.miniproyect3fpoe.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+
+public class WelcomeController  {
+
+    @FXML
+    private Button InstructionsButton;
+
+    @FXML
+    private Button StartGameButton;
+
+    @FXML
+    void InstructionsButtonPressed(ActionEvent event) {
+
+
+    }
+
+    @FXML
+    void HandlePlay(ActionEvent event) throws IOException {
+        
+
+    }
+
+
+}
---
+++ src/main/java/com/example/miniproyect3fpoe/model/Game.java
@@ -0,0 +1,8 @@
+package com.example.miniproyect3fpoe.model;
+
+
+public class Game implements IGame {
+
+
+
+}
