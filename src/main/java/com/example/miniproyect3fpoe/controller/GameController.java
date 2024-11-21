07d3b06c55c685279101941f06996e040b93211d
Juan Natib <juan.natib@correounivalle.edu.co>
2024-11-20 22:30:50 -0500

    PlacementStage update

--- src/main/java/com/example/miniproyect3fpoe/controller/GameController.java
+++ src/main/java/com/example/miniproyect3fpoe/controller/GameController.java
@@ -1,4 +1,16 @@
 package com.example.miniproyect3fpoe.controller;
 
+import com.example.miniproyect3fpoe.model.Game;
+
 public class GameController {
+    private Game game;
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Game getGame() {
+        return game;
+    }
 }
+
